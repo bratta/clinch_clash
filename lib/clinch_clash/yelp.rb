b0e8eb8d452f50f8021689ef95ad9a90cae9c0d9
Tim Gourley <tgourley@gmail.com>
2018-06-14 13:49:01 -0500

    Updating to new Yelp Fusion authentication with API_KEY

--- lib/clinch_clash/yelp.rb
+++ lib/clinch_clash/yelp.rb
@@ -4,14 +4,11 @@ require 'http'
 module ClinchClash
   class Yelp
     def initialize(config = {})
-      @client_id = config[:client_id]
-      @client_secret = config[:client_secret]
+      @api_key = config[:api_key]
 
       @api_host = "https://api.yelp.com"
       @business_path = "/v3/businesses/"
       @search_path = "#{@business_path}search"
-      @token_path = "/oauth2/token"
-      @grant_type = "client_credentials"
 
       @default_term = "dinner"
       @default_location = "73160"
@@ -25,22 +22,8 @@ module ClinchClash
         location: location || @default_location,
         limit: @search_limit
       }
-      response = HTTP.auth(bearer_token()).get(url, params: params)
+      response = HTTP.auth("Bearer #{@api_key}").get(url, params: params)
       response.parse
-    end
-
-    private
-
-    def bearer_token
-      url = "#{@api_host}#{@token_path}"
-      params = {
-        client_id: @client_id,
-        client_secret: @client_secret,
-        grant_type: @grant_type
-      }
-      response = HTTP.post(url, params: params)
-      parsed_response = response.parse
-      "#{parsed_response['token_type']} #{parsed_response['access_token']}"
     end
   end
 end
